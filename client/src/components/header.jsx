770bcd73eb21688bf4e08b96e6cb532e7cfae890
jasperayawan <ejayawan22@gmail.com>
2023-05-18 18:56:16 +0800

    update header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div>
-        <main className="p-[10px] fixed top-0 left-0 w-full">
-          <header className="flex justify-between mb-10 max-w-[1024px] mx-auto py-5">
+        <main className="px-[10px] fixed top-0 left-0 w-full">
+          <header className="flex justify-between mb-10 max-w-[1024px] mx-auto py-5 bg-white">
             <Link to="/" className="font-bold">MyBlog</Link>
             <nav className="flex justify-center items-center gap-4">
               <Link to="/login" className="">Login</Link>
