093b6e35c466e7e54a624eea1412dc3d853da033
jasperayawan <ejayawan22@gmail.com>
2023-05-19 17:59:22 +0800

    update onchange

--- client/src/pages/createPost.jsx
+++ client/src/pages/createPost.jsx
@@ -2,35 +2,49 @@ import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+const modules = {
+    toolbar: [
+        [{ 'header': [1, 2, false] }],
+        ['bold','italic','underline','strike','blockquote'],
+        ['link','image'],
+        ['clean']
+    ]
+};
+
+const formats = [
+    'header',
+    'bold','italic','underline','strike','blockquote',
+    'list','bullet','indent',
+    'link','image'
+];
+
 const CreatePost = () => {
     const [title, setTitle] = useState('');
     const [summary, setsummary] = useState('');
     const [content, setContent] = useState('');
-    const modules = {
-        toolbar: [
-            [{ 'header': [1, 2, false] }],
-            ['bold','italic','underline','strike','blockquote'],
-            ['link','image'],
-            ['clean']
-        ]
-    };
-
-    const formats = [
-        'header',
-        'bold','italic','underline','strike','blockquote',
-        'list','bullet','indent',
-        'link','image'
-    ];
+   
 
   return (
     <div className='max-w-[1024px] mx-auto py-[10rem] px-4'>
         <form>
             <div className='flex flex-col gap-2 mb-2'>
-                <input type='title' placeholder={'Title'}/>
-                <input type="summary" placeholder={'Summary'}/>
+                <input 
+                    type='title' 
+                    placeholder={'Title'} 
+                    value={title} 
+                    onChange={(e) => setTitle(e.target.value)}/>
+                <input 
+                    type="summary" 
+                    placeholder={'Summary'} 
+                    value={summary}
+                    onChange={(e) => setsummary(e.target.value)}/>
                 <input type="file" />
             </div>
-            <ReactQuill value={content} modules={modules} formats={formats}/>
+                <ReactQuill 
+                    value={content} 
+                    modules={modules} 
+                    formats={formats}
+                    onChange={(newValue) => setContent(newValue)}/>
             <div className='mt-2'>
               <button
                 type="submit"
